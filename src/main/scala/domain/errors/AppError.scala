24094ecfd19d02fc9a1155fa8511d67102b0302d
Ivan Vinogradov <vanichvinogradov@yandex.ru>
2024-01-17 15:41:42 +0300

    removed target directories

--- src/main/scala/domain/errors/AppError.scala
+++
@@ -1,8 +0,0 @@
-package domain.errors
-
-sealed trait AppError extends Error {
-  def message: String = "Application error occurred"
-}
-final case class FilmError() extends AppError
-
-final case class DirectorError() extends AppError
